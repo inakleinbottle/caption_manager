df482db286816de2ce9cc0fd91edeb8ca57a524d
Sam Morley <sam@inakleinbottle.com>
2021-12-12 20:12:55 +0000

    Making new dialog

---
+++ src/FilterDialog.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sam on 12/12/2021.
+//
+
+#include "FilterDialog.h"
---
+++ src/FilterDialog.h
@@ -0,0 +1,17 @@
+//
+// Created by sam on 12/12/2021.
+//
+
+#ifndef CAPTION_MANAGER_FILTERDIALOG_H
+#define CAPTION_MANAGER_FILTERDIALOG_H
+
+#include <QtCore>
+#include <QDialog>
+
+
+
+class FilterDialog : public QDialog
+{
+};
+
+#endif//CAPTION_MANAGER_FILTERDIALOG_H
